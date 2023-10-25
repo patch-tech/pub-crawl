0206e835f35053b7364c271b0763a87dcc26abcb
Chris Sims <chris@patch.tech>
2023-10-25 11:13:40 -0700

    it's alive!

--- first_query.ts
+++ first_query.ts
@@ -1,1 +1,15 @@
-import { RestaurantInsights} from "pub-crawl";
+import { RestaurantInsights } from "pub-crawl";
+
+async function main() {
+  let { categoryTags  } = RestaurantInsights.fields;
+
+  let query = RestaurantInsights.select(
+    categoryTags.as("Category_Tags"),
+  ).filter()
+    .limit(10);
+
+  await query.compile().then((data)=> console.log("Compiled query: ", data));
+  await query.execute().then((data)=> console.log(data));
+}
+
+main().catch(console.error);
