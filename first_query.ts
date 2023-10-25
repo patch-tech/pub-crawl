9cc486ce7a249e3bc33dfaba05633dfd80ccd55a
Chris Sims <chris@patch.tech>
2023-10-25 11:38:56 -0700

    moar

--- first_query.ts
+++ first_query.ts
@@ -1,15 +1,32 @@
 import { RestaurantInsights } from "pub-crawl";
+import inquirer from 'inquirer';
 
 async function main() {
-  let { categoryTags  } = RestaurantInsights.fields;
+  let { locationName,
+    categoryTags,
+    latitude,
+    longitude,
+    phoneNumber,
+    streetAddress,
+    medianSpendPerCustomer,
+    openedOn,
+    closedOn } = RestaurantInsights.fields;
 
   let query = RestaurantInsights.select(
-    categoryTags.as("Category_Tags"),
-  ).filter()
+      categoryTags.as("Category_Tags"),
+      locationName.as("Location_Name"),
+      latitude.as("Latitude"),
+      longitude.as("Longitude"),
+      phoneNumber.as("Phone_Number"),
+      streetAddress.as("Street_Address"),
+      medianSpendPerCustomer.as("Median_Spend"),
+      openedOn.as("Opened_On"),
+      closedOn.as("Closed_On"))
+    .filter(categoryTags.like('%Bar or Pub%'))
+    .orderBy
     .limit(10);
 
-  await query.compile().then((data)=> console.log("Compiled query: ", data));
-  await query.execute().then((data)=> console.log(data));
+  await query.execute().then((data) => console.log(data));
 }
 
 main().catch(console.error);
