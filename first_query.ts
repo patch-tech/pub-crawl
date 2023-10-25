78b8de778f5280228e3f1630e33c2702e49aa6b6
Chris Sims <chris@patch.tech>
2023-10-25 12:57:08 -0700

    Get lat and long based on location prompt

--- first_query.ts
+++ first_query.ts
@@ -1,7 +1,68 @@
 import { RestaurantInsights } from "pub-crawl";
-import inquirer from 'inquirer';
+import { input } from '@inquirer/prompts';
+
+type LatLong = {
+  lat: number,
+  lng: number
+};
+
+type Location = {
+  latLng: LatLong
+};
+
+type Result = {
+  locations: Location[]
+};
+
+type LocationResponse = {
+  results: Result[]
+};
+
+
+async function getLatLong(location: string) {
+  const geocode_key = 'AutNw4O37s8NzuL7z0FkBbzZNoAqv2rO';
+  try {
+    const response = await fetch('https://www.mapquestapi.com/geocoding/v1/address?key=' + geocode_key, {
+      method: 'POST',
+      body: JSON.stringify({
+        key: geocode_key,
+        location: location
+      }),
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+
+    const result = (await response.json()) as LocationResponse;
+
+    return result.results[0].locations[0].latLng;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log('error message: ', error.message);
+      console.log('error: ', error);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+}
 
 async function main() {
+
+  const start_location = await input({ message: 'Where are you starting?' });
+  const end_location = await input({ message: 'Where are you stopping?' });
+
+  console.log(start_location);
+  console.log(end_location);
+
+  console.log(await getLatLong(start_location));
+  console.log(await getLatLong(end_location));
+
   let { locationName,
     categoryTags,
     latitude,
@@ -23,10 +84,10 @@ async function main() {
       openedOn.as("Opened_On"),
       closedOn.as("Closed_On"))
     .filter(categoryTags.like('%Bar or Pub%'))
-    .orderBy
     .limit(10);
 
   await query.execute().then((data) => console.log(data));
+
 }
 
 main().catch(console.error);
