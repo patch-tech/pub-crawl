f0646d53c3f5065325f0b48618d291c1baefcd1e
Bill Piel <bill@billpiel.com>
2023-10-25 14:16:42 -0700

    search.ts progress

--- search.ts
+++ search.ts
@@ -1,48 +1,57 @@
 import { RestaurantInsights } from "pub-crawl";
 
 interface IBar {
-	    Latitude?: number,
-	    Longitude?: number,
-	    dist?: number,
-	}
+    Latitude?: number,
+    Longitude?: number,
+    dist?: number,
+}
+
+function to_ratio(a: number, b: number, c: number){
+    return (b - a) / (c - a);
+}
+
+function from_ratio(a: number, r: number, c: number){
+    let d = c - a;
+    return (d * r) + a;
+}
+
 
 async function query_by_latlon(lat0: number, lon0: number, lat1: number, lon1: number) {
 
-    console.log("-");
-    console.log("query_by_latlon");
-    console.log("lat0", lat0);
-    console.log("lon0", lon0);
-    console.log("lat1", lat1);
-    console.log("lon1", lon1);        
+    let min_lat = Math.min(lat0, lat1);
+    let min_lon = Math.min(lat0, lat1);
+    let max_lat = Math.max(lat0, lat1);
+    let max_lon = Math.max(lat0, lat1);
+    
 
     let { locationName,
-    categoryTags,
-    latitude,
-    longitude,
-    phoneNumber,
-    streetAddress,
-    medianSpendPerCustomer,
-    openedOn,
-    closedOn } = RestaurantInsights.fields;
+	  categoryTags,
+	  latitude,
+	  longitude,
+	  phoneNumber,
+	  streetAddress,
+	  medianSpendPerCustomer,
+	  openedOn,
+	  closedOn } = RestaurantInsights.fields;
 
     
     let query = RestaurantInsights.select(
-      categoryTags.as("Category_Tags"),
-      locationName.as("Location_Name"),
-      latitude.as("Latitude"),
-      longitude.as("Longitude"),
-      phoneNumber.as("Phone_Number"),
-      streetAddress.as("Street_Address"),
-      medianSpendPerCustomer.as("Median_Spend"),
+	categoryTags.as("Category_Tags"),
+	locationName.as("Location_Name"),
+	latitude.as("Latitude"),
+	longitude.as("Longitude"),
+	phoneNumber.as("Phone_Number"),
+	streetAddress.as("Street_Address"),
+	medianSpendPerCustomer.as("Median_Spend"),
 	openedOn.as("Opened_On"),
 	closedOn.as("Closed_On"),
     )
 	.filter(categoryTags
 	.like('%Bar or Pub%')
-	.and(latitude.gte(lat0))
-	.and(latitude.lte(lat1))
-	.and(longitude.gte(lon0))
-	.and(longitude.lte(lon1)))
+	.and(latitude.gte(min_lat))
+	.and(latitude.lte(max_lat))
+	.and(longitude.gte(min_lon))
+	.and(longitude.lte(max_lon)))
 	.limit(50);
 
     let bars = (await query.execute()).values();
@@ -52,75 +61,51 @@ async function query_by_latlon(lat0: number, lon0: number, lat1: number, lon1: n
 
 
 async function main() {
-  let { locationName,
-    categoryTags,
-    latitude,
-    longitude,
-    phoneNumber,
-    streetAddress,
-    medianSpendPerCustomer,
-    openedOn,
-    closedOn } = RestaurantInsights.fields;
 
-  // let query = RestaurantInsights.select(
-  //     categoryTags.as("Category_Tags"),
-  //     locationName.as("Location_Name"),
-  //     latitude.as("Latitude"),
-  //     longitude.as("Longitude"),
-  //     phoneNumber.as("Phone_Number"),
-  //     streetAddress.as("Street_Address"),
-  //     medianSpendPerCustomer.as("Median_Spend"),
-  //     openedOn.as("Opened_On"),
-  //     closedOn.as("Closed_On"))
-  //   .filter(categoryTags.like('%Bar or Pub%'))
-  //   .limit(10);
-
-  //   await query.execute().then((data) => console.log(data));
-
-    let lat0 = 40.716110;
-    let lon0 = -74.010790;
-    let lat1 = 40.755760;
-    let lon1 = -73.968979;
-
+    let xlat0 = 40.716110;
+    let xlon0 = -74.010790;
+    let xlat1 = 40.755760;
+    let xlon1 = -73.968979;
     
-    function calc_dist(x: IBar) {
-	console.log("=========");
-	console.log(x.Latitude??0);
-	console.log((x.Latitude??0) - lat0);
-	console.log(lat1-lat0);
+    function calc_dist2 (lat0: number, lon0: number, lat1: number, lon1: number) {
+	return function (x: IBar) {
+	    let dy = Math.abs(((x.Latitude??0) - lat0) / (lat1 - lat0));
+	    let dx = Math.abs(((x.Longitude??0) - lon0) / (lon1 - lon0));
+	    x.dist = dx + dy;
+	    return x;
+	};
+    }
+    
+    let lat =  xlat0;
+    let lon =  xlon0;
 
-	let dy = Math.abs(((x.Latitude??0) - lat0) / (lat1 - lat0));
-	console.log(dy);
-	console.log("------");
+    let n = 5;
+    for (let i = 0; i < n; i++) {
+	let r0 = i / n;
+	let r1 = (i++) / n;
 
-	console.log(x.Longitude??0);
-	console.log(((x.Longitude??0) - lon0));
-	console.log(lon1-lon0);
+	let lat0 = from_ratio(lat, r0, xlat1);
+	let lon0 = from_ratio(lat, r1, lon);
 	
-	let dx = Math.abs(((x.Longitude??0) - lon0) / (lon1 - lon0));
-	console.log(dx);
+	let bars = (await query_by_latlon(lat0, lon0, xlat1, xlon1));
+	let dfn = calc_dist2(lat0, lon0, xlat1, xlon1);
+	let bars0 = [...bars].map(dfn);
+	bars0.sort((a: IBar, b: IBar) => ((a.dist ?? 0) - (b.dist ?? 0)));
+	console.log(bars0[0]);
 	
-	x.dist = dx + dy;
-
-		console.log("vvvvvv");
-
-	console.log(x.dist);
-
-	return x;
     }
 
-    let lat =  lat0 + ((lat1 - lat0) / 10);
-    let lon =  lon0 + ((lon1 - lon0) / 10);    
+    // let bars = (await query_by_latlon(lat0, lon0, lat, lon));
+    // let dfn = calc_dist2(lat0, lon0, lat1, lon1);
+    // let bars0 = [...bars].map(dfn); //.sort((x: IBar) => x.dist);
+    // bars0.sort((a: IBar, b: IBar) => ((a.dist ?? 0) - (b.dist ?? 0)));
+    
+    // //bars0.forEach((x: IBar) => console.log(x));
+    // console.log(bars0.length);
+    // console.log(bars0[0]);
+    // console.log(bars0[10]);
 
-    //await query.execute().then((data) => console.log(data));
-    // let bars =(await query.execute()).values();
-    let bars = (await query_by_latlon(lat0, lon0, lat, lon));
-    let bars0 = [...bars].map(calc_dist); //.sort((x: IBar) => x.dist);
-    bars0.sort((a: IBar, b: IBar) => ((a.dist ?? 0) - (b.dist ?? 0)));
-    //bars0.forEach((x: IBar) => console.log(x));
-    console.log(bars0.length);
-    console.log(bars0[0]);
-    console.log(bars0[10]);
+    
 
 }
 
